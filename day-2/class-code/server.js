fbc36a7f7260d6aedb98255d9f969789430732c9
Jess Eldredge <jess@eldredge.io>
2017-03-29 09:39:08 -0400

    Read directory of game files and game list

---
+++ day-2/class-code/server.js
@@ -0,0 +1,42 @@
+// Use promises to
+//* 1. read the sandwich directory to get a list of files inside
+//* 2. then for each of the files, read the contents of each file
+//* 3. object-ify the contents into a module-level list of games
+
+let TicTacToeGame = require('./src/tic-tac-toe-game');
+let fs = require('fs');
+let BBPromise = require('bluebird');
+let readdir = BBPromise.promisify(fs.readdir);
+let readFile = BBPromise.promisify(fs.readFile);
+let games = [];
+
+function createGameFromJson(json) {
+  let data = JSON.parse(json);
+  let game = new TicTacToeGame({humanFirst: data.humanFirst});
+  game._board._state = data.board;
+  return game;
+} 
+
+function addToGamesList(game) {
+  games.push(game);
+  return games;
+}
+
+function readGameFile(file) {
+  return readFile(`./sandwich/${file}`);
+}
+
+readdir('./sandwich')
+  .then(files => {
+    let processedGames = [];
+    for (let file of files) {
+      let readFiles = readFile(`./sandwich/${file}`, 'utf8')
+        .then(fileContents => createGameFromJson(fileContents))
+        .then(game => addToGamesList(game))
+        .catch(err => console.error(err));
+      processedGames.push(readFiles);
+    }
+    return Promise.all(processedGames);
+  })
+  .then(() => console.log(games))
+  .catch(err => console.error(err));
